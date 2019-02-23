160a45d85f6f764cfa7ff01155acbb6168452e79
Mayuresh Vaze <mavaze@users.noreply.github.com>
2019-02-23 22:06:29 +0100

    Replacing semicolon with colon to make the command runnable on unix

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 #mvn clean ipackage
 
-java -cp "bahubali-core/target/bahubali-core-0.0.1-SNAPSHOT.jar;bahubali-classic/target/bahubali-classic-0.0.1-SNAPSHOT.jar;bahubali-thrones/target/bahubali-thrones-0.0.1-SNAPSHOT.jar;bahubali-build/target/bahubali-build-0.0.1-SNAPSHOT.jar" com.mavaze.puzzles.bahubali.EntryPoint
+java -cp "bahubali-core/target/bahubali-core-0.0.1-SNAPSHOT.jar:bahubali-classic/target/bahubali-classic-0.0.1-SNAPSHOT.jar:bahubali-thrones/target/bahubali-thrones-0.0.1-SNAPSHOT.jar:bahubali-build/target/bahubali-build-0.0.1-SNAPSHOT.jar" com.mavaze.puzzles.bahubali.EntryPoint
